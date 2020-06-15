74d741ec761cef19eeb50b0ea44f3e787132e43a
Cristhian Casierra <rasiek@ip-172-31-26-159.us-east-2.compute.internal>
2020-06-15 20:49:47 +0000

    v0.1 - config filedeleted

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 import config
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 
 app = Flask(__name__)
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
+    print("this works")
+    print(os.environ.get('DATABASE_URI'))
 else:
     app.config.from_object("config.DevelopmentConfig")
 db = SQLAlchemy(app)
+print(db)
 
 print(f'ENV is set to: {app.config["ENV"]}')
 
